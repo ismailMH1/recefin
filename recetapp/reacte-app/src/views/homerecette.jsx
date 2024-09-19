89797e8204e2b954721c073ea20328fe0eb799ac
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2024-09-19 12:00:04 +0100

    update skeleton

--- recetapp/reacte-app/src/views/homerecette.jsx
+++ recetapp/reacte-app/src/views/homerecette.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from '../api/axios';
-import '../App.css'; // Ensure your CSS is imported correctly
+import '../App.css';
 import Nav from '../components/Nav';
 import walletIcon from '../assets/wallet.svg';
 import clockIcon from '../assets/horloge.svg';
@@ -8,6 +8,7 @@ import chefIcon from '../assets/chef.svg';
 
 const HomeRecette = () => {
   const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios.get('/api/recipes')
@@ -22,6 +23,9 @@ const HomeRecette = () => {
       .catch(error => {
         console.error('There was an error fetching the recipes!', error);
         setRecipes([]);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, []);
 
@@ -29,23 +33,33 @@ const HomeRecette = () => {
     <div className="container">
       <Nav />
       <h2>Recettes</h2>
-      <p className="first-paragraph">Parcourez notre sélection pour découvrir de nouvelles recettes passionnantes que vous pouvez essayer chez vous.</p>      <div className="recipe-list">
-        {recipes.map((recipe) => (
-          <div className="recipe-card" key={recipe.id}>
-            <img src={`http://127.0.0.1:8000/images/${recipe.image}`} alt={recipe.title} className="recipe-image" />
-            <div className="recipe-info">
-              <div className="recipe-meta">
-                <span><img src={clockIcon} alt="Duration" className="icon" /> {recipe.duration}</span>
-                <span><img src={chefIcon} alt="Difficulty" className="icon" /> {recipe.difficulty}</span>
-                <span><img src={walletIcon} alt="Budget" className="icon" /> {recipe.budget}</span>
+      <p className="first-paragraph">Parcourez notre sélection pour découvrir de nouvelles recettes passionnantes que vous pouvez essayer chez vous.</p>
+      <div className="recipe-list">
+        {loading ? (
+         
+          <>
+            <div className="skeleton-card skeleton"></div>
+            <div className="skeleton-card skeleton"></div>
+            <div className="skeleton-card skeleton"></div>
+          </>
+        ) : (
+          recipes.map((recipe) => (
+            <div className="recipe-card" key={recipe.id}>
+              <img src={`http://127.0.0.1:8000/images/${recipe.image}`} alt={recipe.title} className="recipe-image" />
+              <div className="recipe-info">
+                <div className="recipe-meta">
+                  <span><img src={clockIcon} alt="Duration" className="icon" /> {recipe.duration}</span>
+                  <span><img src={chefIcon} alt="Difficulty" className="icon" /> {recipe.difficulty}</span>
+                  <span><img src={walletIcon} alt="Budget" className="icon" /> {recipe.budget}</span>
+                </div>
+                <p className="recipe-description">{recipe.description}</p>
+                <a href={`/recipe/${recipe.id}`} className="discover-button">
+                  Découvrir <i className="fas fa-arrow-right"></i>
+                </a>
               </div>
-              <p className="recipe-description">{recipe.description}</p>
-              <a href={`/recipe/${recipe.id}`} className="discover-button">
-                Découvrir <i className="fas fa-arrow-right"></i>
-              </a>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
